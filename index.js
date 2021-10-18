6b283903e5a00d45a86161461af375b5fa914e37
arturguerra921 <arturguerra921@hotmail.com>
2021-10-18 11:54:33 -0300

    Remover Acentos 4

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ client.on('ready', () => {
 
 // Função responsável por trocar mensagens com o seu cliente.
 client.on('message', msg =>{
-    const tratando_string = msg.body.toLowerCase(); // Fiz esse tratamento pensando em facilitar o envio de mensagens dos clientes que agora podem enviar tudo maiusculo, minusculo ou "mesclado" que o nosso bot vai compreender.  
+
+    //const tratando_string = msg.body.toLowerCase(); // Fiz esse tratamento pensando em facilitar o envio de mensagens dos clientes que agora podem enviar tudo maiusculo, minusculo ou "mesclado" que o nosso bot vai compreender.  
+    const tratando_string = msg.body.normalize("NFD").replace(/\p{Diacritic}/gu, "");
     
     // Remover acentos - [Artur] - https://stackoverflow.com/questions/990904/remove-accents-diacritics-in-a-string-in-javascript
     //tratando_string = tratando_string.normalize("NFD").replace(/\p{Diacritic}/gu, "");
