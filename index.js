ccc7574a73393edd9c10d929847ac04cd7e5de2f
Lucas Caetano <60528504+LucasMSCaetano@users.noreply.github.com>
2025-03-08 10:03:31 -0300

    Update index.js
    change on phone number

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ client.on('message', msg =>{
 
         case "2":
         case "!2":
-            msg.reply('Você escolheu a *_opção 2 Referente a Filiação/Grêmio no batalhão BPEB_*\n\nAgora para comprar um voucher de internet no BPEB é obrigatório que você seja filiado no grêmio, seguem os contatos para que você resolva essas questões:\n\n\n1º Ten Junior: *(61) 99859-0409*\n3º Sgt Josiel: *(61) 99304-9635*');        
+            msg.reply('Você escolheu a *_opção 2 Referente a Filiação/Grêmio no batalhão BPEB_*\n\nAgora para comprar um voucher de internet no BPEB é obrigatório que você seja filiado no grêmio, seguem os contatos para que você resolva essas questões:\n\n\n1º Ten Junior: *(61) 99859-0409*\n3º Sgt Bandeira: *(61) 99193-4866*');        
             break;
 
         case "3":
